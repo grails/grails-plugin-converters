7f15ad27a117ba6aaa8536f66df7e0792a9ca496
jameskleeh <james.kleeh@gmail.com>
2017-03-30 11:12:13 -0400

    Add codecs, gradle wrapper, travis setup

---
+++ travis-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+rm -rf *.zip
+./gradlew clean check assemble || EXIT_STATUS=$?
+
+EXIT_STATUS=0
+echo "Publishing archives for branch $TRAVIS_BRANCH"
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+  echo "Publishing archives"
+
+  if [[ -n $TRAVIS_TAG ]]; then
+      ./gradlew bintrayUpload || EXIT_STATUS=$?
+  else
+      ./gradlew publish || EXIT_STATUS=$?
+  fi
+
+fi
