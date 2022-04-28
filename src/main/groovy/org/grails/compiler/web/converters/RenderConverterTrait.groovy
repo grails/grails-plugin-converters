cebb21a6a86dd25311197cd45132ff754deb64dc
Alar Aule <alar.aule@smit.ee>
2022-04-28 21:11:21 +0300

    grails-core issue https://github.com/grails/grails-core/issues/11318 apply Generated annotation to RenderConverterTrait method, to improve test coverage reports

--- src/main/groovy/org/grails/compiler/web/converters/RenderConverterTrait.groovy
+++ src/main/groovy/org/grails/compiler/web/converters/RenderConverterTrait.groovy
@@ -1,6 +1,7 @@
 package org.grails.compiler.web.converters
 
 import grails.artefact.Enhances
+import groovy.transform.Generated
 import org.grails.web.converters.Converter
 import org.grails.web.servlet.mvc.GrailsWebRequest
 import org.springframework.web.context.request.RequestContextHolder
@@ -15,6 +16,7 @@ trait RenderConverterTrait {
      *
      * @param converter The converter to render
      */
+    @Generated
     void render(Converter<?> converter) {
         GrailsWebRequest webRequest = (GrailsWebRequest)RequestContextHolder.currentRequestAttributes()
         HttpServletResponse response = webRequest.currentResponse
