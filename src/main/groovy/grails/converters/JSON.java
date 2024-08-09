522b0a8fa195f333f31a7345354d9dc072742fe9
Mattias Reichel <mattias.reichel@gmail.com>
2024-08-09 23:26:34 +0200

    feat!: Update to Grails 7
    - Java 17
    - Groovy 4
    - Jakarta Servlet

--- src/main/groovy/grails/converters/JSON.java
+++ src/main/groovy/grails/converters/JSON.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2006-2007 Graeme Rocher
+ * Copyright 2006-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -34,8 +34,8 @@ import org.grails.web.converters.marshaller.ClosureObjectMarshaller;
 import org.grails.web.converters.marshaller.ObjectMarshaller;
 import org.grails.web.json.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.*;
 
--- src/main/groovy/org/grails/compiler/web/converters/RenderConverterTrait.groovy
+++ src/main/groovy/org/grails/compiler/web/converters/RenderConverterTrait.groovy
@@ -6,7 +6,7 @@ import org.grails.web.converters.Converter
 import org.grails.web.servlet.mvc.GrailsWebRequest
 import org.springframework.web.context.request.RequestContextHolder
 
-import javax.servlet.http.HttpServletResponse
+import jakarta.servlet.http.HttpServletResponse
 
 @Enhances(["Controller", "Interceptor"])
 trait RenderConverterTrait {
